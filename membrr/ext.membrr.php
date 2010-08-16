f9f90d362f808da9a88d7df897c8849fc526ed42
brockf <brockf@356de91c-0bb9-460c-8fbf-084137d4394a>
2010-08-16 01:29:07 +0000

    Updated extension to work with EE2.1.0

--- membrr/ext.membrr.php
+++ membrr/ext.membrr.php
@@ -55,26 +55,11 @@ class Membrr_ext
 	
     function activate_extension()
 	{
-		//	----------------------------------
-		//	SQL add field
-		//	----------------------------------
-		$insert_vars = array(
-						'extension_id' => '',
-						'class'        => $this->ext_class,
-						'method'       => 'saef_form_membrr', // Redirect to Order Form?
-						'hook'         => 'channel_standalone_form_start',
-						'settings'     => '',
-						'priority'     => 10,
-						'version'      => $this->version,
-						'enabled'      => 'y'
-					);
-		$this->EE->db->insert('exp_extensions',$insert_vars);
-		
 		$insert_vars = array(
 						'extension_id' => '',
 						'class'        => $this->ext_class,
 						'method'       => 'saef_insert_membrr', // Record entry_id for subscription
-						'hook'         => 'channel_standalone_insert_entry',
+						'hook'         => 'entry_submission_redirect',
 						'settings'     => '',
 						'priority'     => 10,
 						'version'      => $this->version,
@@ -107,33 +92,6 @@ class Membrr_ext
 	{
 	    $this->EE->db->delete('exp_extensions',array('class' => $this->ext_class));
 	}
-	
-	/*
-	* saef_form_membrr
-	*
-	* Redirect the user to the order form if they don't have an active subscription for this post
-	*
-	* @param string $return_form The entry form
-	* @param string $catpcha The CAPTCHA
-	* @param string $channel_id The numeric ID of the current channel
-	* @return string The entry form if they are OK.  Redirects to order form if not OK.
-	*/
-    function saef_form_membrr ($return_form, $captcha, $channel_id) {
-    	/**
-		* Removed because we don't want to redirect when people are editing
-		* If this works for you, you can include it
-    	// are we protecting this?
-    	if ($channel = $this->membrr->GetChannel($channel_id, 'exp_membrr_channels.channel_id')) {    		    		
-    		// yes we are, does this person have an active subscription?
-    		if (!$this->membrr->GetSubscriptionForChannel($this->EE->session->userdata('member_id'),$channel['plans'],$channel['one_post'])) {
-    			// nope, redirect to order form
-    			header('Location: ' . $channel['order_form']);
-    			die();
-    		}
-    	} */
-    	
-    	return TRUE;
-    }
     
     /*
 	* saef_insert_membrr
@@ -142,46 +100,43 @@ class Membrr_ext
 	*
 	* @return boolean TRUE upon success
 	*/
-    function saef_insert_membrr () {
+    function saef_insert_membrr ($entry_id, $meta_data, $post_data, $cp_call, $orig_loc) {
+    	// we don't care about CP posts
+    	if ($cp_call == TRUE) {
+    		return $orig_loc;
+    	}
+    
+    	// we don't care if they aren't logged in
     	if (!$this->EE->session->userdata('member_id')) {
-    		return FALSE;
+    		return $orig_loc;
     	}
     	
-    	if (isset($_POST['entry_id'])) {
+    	// are we editing?
+    	$this->EE->db->where('channel_entry_id',$entry_id);
+    	$result = $this->EE->db->get('exp_membrr_channel_posts');
+    	
+    	if ($result->num_rows() > 0) {
     		// we are editing
-    		return FALSE;
+    		return $orig_loc;
     	}
-    	if (!isset($_POST['channel_id']) or !is_numeric($_POST['channel_id'])) {
-    		return FALSE;
-    	}
-    	
-    	$_POST['channel_id'] = mysql_real_escape_string($_POST['channel_id']);
     	
     	// are we protecting this?
-    	if ($channel = $this->membrr->GetChannel($_POST['channel_id'],'exp_membrr_channels.channel_id')) {
+    	if ($channel = $this->membrr->GetChannel($meta_data['channel_id'],'exp_membrr_channels.channel_id')) {
     		// yes we are, does this person have an active subscription?
     		
     		// we may be passed a specific subscription ID
     		$sub_id = (isset($_POST['subscription_id']) and !empty($_POST['subscription_id']) and is_numeric($_POST['subscription_id'])) ? $_POST['subscription_id'] : FALSE;
-    		if (!$recurring_id = $this->membrr->GetSubscriptionForChannel($SESS->userdata['member_id'],$channel['plans'],$channel['one_post'], $sub_id)) {
+    		if (!$recurring_id = $this->membrr->GetSubscriptionForChannel($this->EE->session->userdata('member_id'),$channel['plans'],$channel['one_post'], $sub_id)) {
     			// nope, redirect to order form
-    			header('Location: ' . $channel['order_form']);
-    			die();
+    			return $channel['order_form'];
     		}
     		else {
     			// link with $recurring_id
-    			
-    			// get next insert ID
-    			$result = $this->EE->db->query("SHOW TABLE STATUS LIKE 'exp_channel_titles'");
-				$row = $result->row_array();
-				
-				$insert_id = $row['Auto_increment'];
-				
-    			$insert = $this->EE->db->query('INSERT INTO `exp_membrr_channel_posts` (`channel_id`, `channel_entry_id`, `recurring_id`, `active`, `channel_post_date`) VALUES (\'' . $channel['channel_id'] . '\', \'' . $insert_id . '\', \'' . $recurring_id . '\', \'1\', \'' . date('Y-m-d H:i:s') . '\');');
+    			$this->EE->db->query('INSERT INTO `exp_membrr_channel_posts` (`channel_id`, `channel_entry_id`, `recurring_id`, `active`) VALUES (\'' . $channel['channel_id'] . '\', \'' . $entry_id . '\', \'' . $recurring_id . '\', \'1\');');
     		}
     	}
     	
-    	return TRUE;
+    	return $orig_loc;
     }
 }
 
