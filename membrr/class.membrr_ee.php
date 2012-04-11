d439d5e9b806268d686aa52b14d377c2c1304f07
brockf <brockf@356de91c-0bb9-460c-8fbf-084137d4394a>
2012-04-11 15:47:44 +0000

    [public] Update renewed_recurring_id when someone runs UpdateCC.

--- membrr/class.membrr_ee.php
+++ membrr/class.membrr_ee.php
@@ -482,6 +482,7 @@ if (!class_exists('Membrr_EE')) {
 			if (isset($response['response_code']) and $response['response_code'] == '104') {
 				// we have to update all local subscription ID references
 				$this->EE->db->update('exp_membrr_subscriptions',array('recurring_id' => $response['recurring_id']),array('recurring_id' => $subscription_id));
+				$this->EE->db->update('exp_membrr_subscriptions',array('renewed_recurring_id' => $response['recurring_id']),array('renewed_recurring_id' => $subscription_id));
 				$this->EE->db->update('exp_membrr_payments',array('recurring_id' => $response['recurring_id']),array('recurring_id' => $subscription_id));
 				$this->EE->db->update('exp_membrr_channel_posts',array('recurring_id' => $response['recurring_id']),array('recurring_id' => $subscription_id));
 				
