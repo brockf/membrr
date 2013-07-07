535bd9052a0f5fa587f1d237a8940f321e75601c
Brock Ferguson <brock.ferguson@gmail.com>
2013-07-07 18:58:02 -0500

    Updated dashboard template for new Reports module.

--- membrr/views/dashboard.php
+++ membrr/views/dashboard.php
@@ -17,16 +17,67 @@
 $this->table->set_template($cp_pad_table_template); // $cp_table_template ?
 
 $this->table->set_heading(
-    array('data' => 'Configure Report', 'colspan' => '4')
+    array('data' => 'Select Date Range and Subscription Plan', 'colspan' => '4')
 );
 
 
 $this->table->add_row(
 				array('style' => 'width: 25%', 'data' => 'Start Date: ' . form_dropdown('start_month', $months, $start_month) . ' ' . form_dropdown('start_day', $days, $start_day) . ' ' . form_dropdown('start_year', $years, $start_year)),
-				array('style' => 'width: 15%', 'data' => 'End Date: ' . form_dropdown('end_month', $months, $end_month) . ' ' . form_dropdown('end_day', $days, $end_day) . ' ' . form_dropdown('end_year', $years, $end_year)),
+				array('style' => 'width: 25%', 'data' => 'End Date: ' . form_dropdown('end_month', $months, $end_month) . ' ' . form_dropdown('end_day', $days, $end_day) . ' ' . form_dropdown('end_year', $years, $end_year)),
 				array('colspan' => '2', 'data' => '')
 			);
 			
+$this->table->add_row(
+				array('colspan' => '2', 'style' => 'width: 25%', 'data' => 'Subscription Plan: ' . form_dropdown('plan_id', $plan_options, $current_plan)),
+				array('colspan' => '2', 'data' => '')
+			);			
+			
+$this->table->add_row(
+				array('colspan' => '4', 'style' => 'width: 100%', 'data' => form_submit('report', 'Refresh Report'))
+			);						
+
+?>
+
+<?=$this->table->generate();?>
+<?=$this->table->clear();?>
+
+<?php
+
+$this->table->set_template($cp_pad_table_template); // $cp_table_template ?
+
+$this->table->set_heading(
+    array('data' => 'Total Subscriptions at End of Date Range (' . date('F d, Y', strtotime($end_year . '-' . $end_month . '-' . $end_day)) . ')', 'colspan' => '4')
+);
+
+foreach ($plans as $plan) {
+	if ($current_plan == $plan['id'] or $current_plan == 0) {
+		$total = isset($plan_totals[$plan['id']]) ? $plan_totals[$plan['id']] : 0;
+		
+		$occurrences = ($plan['occurrences'] == 0) ? 'infinite' : $plan['occurrences'] . ' charges';
+		$plan_name = $plan['name'] . ' (' . $plan['interval'] . ' days | ' . $occurrences . ')';
+					
+		$this->table->add_row(
+						array('width' => '50%', 'data' => $plan_name),
+						array('width' => '50%', 'data' => $total)
+					);			
+	}
+}
+
+?>
+
+<?=$this->table->generate();?>
+<?=$this->table->clear();?>
+
+<?=form_open($reports_action);?>
+
+<?php
+
+$this->table->set_template($cp_pad_table_template); // $cp_table_template ?
+
+$this->table->set_heading(
+    array('data' => 'Select Report Type', 'colspan' => '4')
+);
+
 $this->table->add_row(
 				array('style' => 'width: 25%', 'data' => form_radio('show','subscriptions',($show == 'subscriptions') ? TRUE : FALSE) . ' Subscriptions (' . $count_subscriptions . ')'),
 				array('style' => 'width: 25%', 'data' => form_radio('show','renewals',($show == 'renewals') ? TRUE : FALSE) . ' Renewals (' . $count_renewals . ')'),
@@ -110,4 +161,4 @@ else {
 
 <?=$this->table->generate();?>
 <?=$this->table->clear();?>
-<?=$pagination;?> 
+<?=$pagination;?>
